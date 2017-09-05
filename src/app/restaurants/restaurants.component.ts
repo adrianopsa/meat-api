6ba8203e54a0d70244d8cadedbf5fc36456fc765
adriano.psa <adriano.psa@gmail.com>
2017-09-05 13:53:20 -0300

    Finished

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations';
+import { FormBuilder, FormControl, FormGroup} from '@angular/forms'
+
 import { Restaurant } from './restaurant/restaurant.model'
 import { RestaurantsService} from './restaurants.service'
 
-import { trigger, state, style, transition, animate} from '@angular/animations';
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+
+import { Observable } from 'rxjs/Observable'
+
+
 
 @Component({
   selector: 'mt-restaurants',
@@ -28,11 +40,30 @@ export class RestaurantsComponent implements OnInit {
 
   searchBarState = 'hidden'
   restaurants: Restaurant[]
+  searchForm : FormGroup
+  searchControl: FormControl
 
-
-  constructor(private restaurantsService: RestaurantsService) { }
+  constructor(
+      private restaurantsService: RestaurantsService, 
+      private formBuilder: FormBuilder
+    ) { }
 
   ngOnInit() {
+
+    this.searchControl = this.formBuilder.control('')
+
+    this.searchForm = this.formBuilder.group({
+      searchControl: this.searchControl
+    })
+
+    this.searchControl.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .switchMap(searchTerm => 
+                  this.restaurantsService.restaurants(searchTerm)
+                  .catch(error=> Observable.from([])))
+                  .subscribe(restaurants => this.restaurants = restaurants)
+
     this.restaurantsService.restaurants()
             .subscribe(restaurants => this.restaurants = restaurants)
   }
